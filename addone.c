abcbe8c99c36c92a1907c82ee306ed45b7ff3887
chandanacm123 <cmchandana377@gmail.com>
2019-05-06 12:14:21 +0530

    Create addone.c

---
+++ addone.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int get_input(int a)
+{
+     int a;
+     printf("Enter a number: \n");
+     scanf("%d",&a);
+     return a;
+}
+int add_one(int a)
+{
+      int b;
+      b=a+1
+      return b;
+}
+void output(int a, int b)
+int main(void)
+{
+    int a=get_input();
+    int b=add_one(a);
+}
+
